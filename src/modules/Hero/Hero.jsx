bb644e7d0631bf32f7f6b3ba07002a1e103d316d
TrueMolni <truemolni@gmail.com>
2023-08-29 19:30:13 +0300

    feat: :art: add example pf animation

--- src/modules/Hero/Hero.jsx
+++ src/modules/Hero/Hero.jsx
@@ -1,23 +1,32 @@
-import Button from "../../shared/components/Button";
-import Title from "../../shared/components/Title";
+// import { useRef } from 'react';
+// import Lottie, { LottieRefCurrentProps } from 'lottie-react';
 
-import css from "./hero.module.css";
-//
-// Anjhay
-//
+import Button from '../../shared/components/Button';
+import Title from '../../shared/components/Title';
+// import animation from '../../shared/images/lottie/flower.json';
+
+import css from './hero.module.css';
 const Hero = () => {
+  // const animationRef = useRef < LottieRefCurrentProps > null;
   return (
     <section className={css.bg}>
-      <div className={css.contentWrapper + " container"}>
+      <div className={css.contentWrapper + ' container'}>
         <Title
           customClass={css.mainTitle}
-          title={"Flower Décor Company"}
-          size={"h1"}
+          title={'Flower Décor Company'}
+          size={'h1'}
         />
         {/* <h2 className={css.accentTitle}>
           Світ квітів, декору і краси з історією.
         </h2> */}
-        <Button customClass={css.heroBtn} text={"Замовити"} />
+        <Button customClass={css.heroBtn} text={'Замовити'} />
+        {/* <div style={{ width: '300px' }}>
+          <Lottie
+            onComplete={() => {}}
+            lottieRef={animationRef}
+            animationData={animation}
+          />
+        </div> */}
       </div>
     </section>
   );
