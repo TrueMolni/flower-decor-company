63d1a2266d63148d594594c0d1205980e7ad4031
TrueMolni <truemolni@gmail.com>
2023-09-18 15:32:39 +0300

    feat: :sparkles: add select theme button
    work in progress

--- src/modules/SharedLayout/SharedLayout.jsx
+++ src/modules/SharedLayout/SharedLayout.jsx
@@ -1,7 +1,6 @@
-import { Outlet } from "react-router-dom";
-import Header from "../Header";
-import Footer from "../Footer";
-
+import { Outlet } from 'react-router-dom';
+import Header from '../Header';
+import Footer from '../Footer';
 // import css from './shared-layout.module.css';
 
 const SharedLayout = () => {
